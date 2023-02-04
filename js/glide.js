1ed50a556bde96c842d4f2e6b6c50dd4d8e86bc5
erenGuden <105981672+erenGuden@users.noreply.github.com>
2023-02-03 16:28:59 -0800

    fix the code and github

--- js/glide.js
+++ js/glide.js
@@ -1,7 +1,37 @@
 const config = {
   perView: 4,
-  gap: 4,
-  autoplay: 3000, 
+  gap: 20,
+  autoplay: 3000,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
 };
 
 new Glide(".product-carousel", config).mount();
+
+const config2 = {
+  perView: 4,
+  gap: 20,
+  autoplay: 3000,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".product-carousel2", config2).mount();
