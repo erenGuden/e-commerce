a473d156c1772c3507525bc295c92e6f5c47780e
erenGuden <105981672+erenGuden@users.noreply.github.com>
2023-02-16 02:14:50 -0800

    Logic implementation

--- js/single-product.js
+++ js/single-product.js
@@ -2,6 +2,9 @@ import { thumbsActiveFunction } from "./single-product/thumbsActive.js";
 import zoomFunc from "./single-product/zoom.js";
 import colorsFunc from "./single-product/colors.js";
 import valuesFunc from "./single-product/values.js";
+import tabsFunc from "./single-product/tabs.js";
+import commentsFunc from "./single-product/comments.js";
+
 
 const productId = localStorage.getItem("productId")
   ? JSON.parse(localStorage.getItem("productId"))
@@ -44,3 +47,23 @@ const productThumbs = document.querySelectorAll(
 );
 
 productThumbs[0].classList.add("active");
+
+// Add to cart
+let cart = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+
+const findCart = cart.find((item) => item.id === findProduct.id);
+const btnAddToCart = document.getElementById("add-to-cart");
+const quantityDOM = document.getElementById("quantity");
+let cartItems = document.querySelector(".header-cart-count");
+if (findCart) {
+  btnAddToCart.setAttribute("disabled", "disabled");
+} else {
+  btnAddToCart.addEventListener("click", function () {
+    cart.push({ ...findProduct, quantity: Number(quantityDOM.value) });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    btnAddToCart.setAttribute("disabled", "disabled");
+    cartItems.innerHTML = cart.length;
+  });
+}
